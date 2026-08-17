6c98a584398a70997d2a0835defdf5d82612ea5b
Mateus Costa <mateus-cc@outlook.com>
2026-08-17 19:11:43 -0300

    feat: estrutura inicial do projeto (Spring + React + Docker + Docs)

---
+++ backend/src/main/java/com/barbearia/babearia_api/BabeariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.barbearia.babearia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabeariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabeariaApiApplication.class, args);
+	}
+
+}
