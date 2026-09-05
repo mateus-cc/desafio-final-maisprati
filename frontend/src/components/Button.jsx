e322876ff0758562247e7aec82b9a0554ad8ca6a
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-09-04 21:58:26 -0300

    fix: corrigir classe de arredondamento do botão para rounded-xl

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 export function Button({children}) {
   return (
-    <button className="bg-gold-500 text-gray-500 w-full rounded p-2">
+    <button className="bg-gold-500 text-gray-500 w-full rounded-xl p-2">
       {children}
     </button>
   );
