668ea19c23bf25dbe36b63438c676161fd94eecb
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2026-09-11 15:05:56 -0300

    feat: adicionar teste para delegação do método main à SpringApplication

--- backend/src/test/java/com/barbearia/babearia_api/BabeariaApiApplicationTests.java
+++ backend/src/test/java/com/barbearia/babearia_api/BabeariaApiApplicationTests.java
@@ -1,6 +1,9 @@
 package com.barbearia.babearia_api;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +11,19 @@ class BabeariaApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void mainDelegatesToSpringApplication() {
+		String[] arguments = {"--spring.main.web-application-type=none"};
+
+		try (var springApplication = mockStatic(SpringApplication.class)) {
+			BabeariaApiApplication.main(arguments);
+
+			springApplication.verify(
+				() -> SpringApplication.run(BabeariaApiApplication.class, arguments),
+				times(1));
+		}
 	}
 
 }
